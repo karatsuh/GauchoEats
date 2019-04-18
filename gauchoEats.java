93c0148c29ab3eacb1309fc77d1d133810e1f7ac
Lidi Yafei <lidi@169-231-80-164.wireless.ucsb.edu>
2019-04-18 01:01:57 -0700

    Test for Java test

--- gauchoEats.java
+++ gauchoEats.java
@@ -6,3 +6,13 @@ public class gauchoEats {
         return "Hello world!";
     }
 }
+
+// import org.junit.jupiter.api.Test;
+ 
+// class JUnit5ExampleTest {
+ 
+//     @Test
+//     void justAnExample() {
+//         System.out.println("This test method should be run");
+//     }
+// }
---
+++ test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+// test.java
+
+public class test {
+    @Test
+    public static void main(String[] args) {
+        assertEquals(gauchoEats.helloWorld(), "Hello world!");
+    }
+}
+// @Test
+// public void testAdd() {
+//     assertEquals(42, Integer.sum(19, 23));
+// }
