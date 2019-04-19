9ef9e1dec164a6ccdb519a99e25353f68926f08f
lidiyf <35703471+lidiyf@users.noreply.github.com>
2019-04-19 05:32:46 -0700

    Update GauchoEatsTest.java
    Fix error

--- src/test/java/GauchoEatsTest.java
+++ src/test/java/GauchoEatsTest.java
@@ -1,8 +1,8 @@
-// import org.junit.jupiter.api.Test;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+// import static org.junit.Assert.assertEquals;
+// import org.junit.Test;
 
 public class GauchoEatsTest {
 
