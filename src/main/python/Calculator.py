f92a04f1f118e5f8a5d6356793217e0a1f674a61
lidiyf <35703471+lidiyf@users.noreply.github.com>
2019-04-19 09:03:44 -0700

    Create Calculator.py

---
+++ src/main/python/Calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError('divisor cannot be 0')
+        return a / b
