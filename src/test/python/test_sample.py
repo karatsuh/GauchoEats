725b05e96088f921e7c6f796790d5a725d08ba42
lidiyf <35703471+lidiyf@users.noreply.github.com>
2019-04-19 05:13:58 -0700

    Create test_sample.py

---
+++ src/test/python/test_sample.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'main', 'python'))
+
+from imageRecognition import helloWorld
+
+import pytest
+
+def test_helloWorld():
+    assert helloWorld() == "Hello world!"
