776325d89da1c51f93d7005c43359950cb7e5f09
lidiyf <35703471+lidiyf@users.noreply.github.com>
2019-04-19 06:50:11 -0700

    Update and rename GauchoEatsTest.java to GauchoeatsTest.java

---
+++ src/test/java/GauchoeatsTest.java
@@ -0,0 +1,11 @@
+import static org.junit.assertEquals;
+import org.junit.Test;
+
+public class GauchoeatsTest {
+
+    @Test
+    public void helloWorld() {
+        String output = gauchoEats.helloWorld();
+        assertEquals(output, "Hello world!");
+    }
+}
