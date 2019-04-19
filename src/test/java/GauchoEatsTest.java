69f0cb971442eb31a5fa8e14433ec15f03da2f4e
lidiyf <35703471+lidiyf@users.noreply.github.com>
2019-04-19 05:25:45 -0700

    Update and rename test_GauchoEats to GauchoEatsTest.java
    Try to fail java test

---
+++ src/test/java/GauchoEatsTest.java
@@ -0,0 +1,14 @@
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GauchoEatsTest {
+
+    @Test
+    public void helloWorld() {
+        assertEquals(2, 33);
+        // assertEquals(gauchoEats.helloWorld(), "Hello Lidi!");
+    }
+}
