736198a6b21ecb2f5e0c172674c34670c9bac172
zbaral <38017485+zbaral@users.noreply.github.com>
2019-04-30 02:24:43 -0700

    Update lambda_function.py

--- src/main/python/lambda_function.py
+++ src/main/python/lambda_function.py
@@ -5,10 +5,16 @@ import boto3 #AWS SDK for python
 client = boto3.resource('dynamodb')
 table = client.Table('GauchoEats')
 
-#def dynamoGet(DiningCommon, metric):
-    #demand a metric(returns a string) for a DiningCommon
-    #WIP
-
+def dynamoGet(DiningCommon, metric):
+    #PreCondition: DiningCommon and metric are both strings
+    #PostCondition: returns the wanted metric as a string
+    #DiningCommon = "dlg","carrillo","ortega"
+    #metric = "capacity","line"
+    dynamoResponse = table.get_item(Key = {'DiningCommon' : DiningCommon})
+    metric = dynamoResponse['Item'][metric]
+    metric = str(metric)
+    return metric
+    
 # Original (Does't work after adding skill card)
 # def createResponse(speech, endSession):
 #     #returns json back to alexa for it to parse an appropriate response
@@ -45,6 +51,7 @@ def createSkillCard(title, content):
     card['title'] = title
     card['content'] = content
     return card
+    
 
 def on_session_started(session_started_request, session):
         print ("Starting new session.")
@@ -66,92 +73,74 @@ def lambda_handler(event, context):
         return createResponse(speech, False)
         
     if event['request']['intent']['name'] == "leastCrowded":
-        #compare dining commons and store dining common and its capacity
-        
-        dynamoResponse = table.get_item(Key = {'DiningCommon' : 'dlg'})
-        capacity = str(dynamoResponse['Item']['line'])
-        dlg = ("dlg", capacity)
-        '''
-        dynamoResponse = table.get_item(Key = {'DiningCommon' : 'carrillo'})
-        capacity = dynamoResponse['Item']['line']
-        carrillo = make_pair("carrillo", capacity)
-        dynamoResponse = table.get_item(Key = {'DiningCommon' : 'ortega'})
-        capacity = dynamoResponse['Item']['line']
-        ortega = make_pair("ortega", capacity)
-        '''
-        #if dlg.second < carrillo.second and dlg.second < ortega.second
-        speech = buildSpeech("The least crowded dining common is " + dlg[0] + " with capacity " + dlg[1])
+        #compare dining commons and store dining common name and its capacity
+        dlg = ("dlg", dynamoGet("dlg", "capacity"))
+        carrillo = ("carrillo", dynamoGet("carrillo", "capacity"))
+        ortega = ("ortega", dynamoGet("ortega", "capacity"))
+        #in progress
+        if (dlg[1] == 0) and (carrillo[1] == 0) and (ortega[1] == 0):
+            speech = buildSpeech("The dining commons are closed")
+        else:
+            if (dlg[1] <= carrillo[1]) and (dlg[1] <= ortega[1]):
+                leastCrowded = (dlg[0], dlg[1])
+            elif (carrillo[1] <= dlg[1]) and (carrillo[1] <= ortega[1]):
+                leastCrowded = (carrillo[0], carrillo[1])
+            else:
+                leastCrowded = (ortega[0], ortega[1])
+            speech = buildSpeech("The least crowded dining common is " + leastCrowded[0] + " with capacity " + leastCrowded[1])
         #skill card
-        line = str(dynamoResponse['Item']['line'])
-        skillCardTitle = "More Informatiom on De La Guerra"
-        skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
+        skillCardTitle = "Which Dining Hall is Least Crowded!?"
+        skillCardContent = dlg[1] + " people in DLG\n" + ortega[1] + " people in Ortega\n" + carrillo[1] + " people in Carrillo\n"
+        skillCardContent += "It will take only 5 min to get to DLG and 20 min to finish meal!"
         skillCard = createSkillCard(skillCardTitle, skillCardContent)
         return createResponse(speech, True, skillCard)
     
     if event['request']['intent']['name'] == "getCapacity":
         speech = buildSpeech("getCapacity request received, where's the slot?!")
         if event['request']['intent']['slots']['diningCommon']['value'] == "dlg":
-            dynamoResponse = table.get_item(Key = {'DiningCommon' : 'dlg'})
-            capacity = dynamoResponse['Item']['line']
-            capacity = str(capacity)
+            capacity = dynamoGet("dlg", "capacity")
             speech = buildSpeech("De La Guerra has a capacity of: " + capacity)
             #skill card idk if we want to change
-            line = str(dynamoResponse['Item']['line'])
-            skillCardTitle = "More Information on De La Guerra"
-            skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
-            skillCard = createSkillCard(skillCardTitle, skillCardContent)
+            line = dynamoGet("dlg", "line")
+            skillCardTitle = "About De La Guerra"
+            skillCardContent = "There are " + line + " people in line.\n\nCongestion: " + "40%" + "\nAverage Eating Time: " + "25" + " min\nMenu: " + "burrito"
         elif event['request']['intent']['slots']['diningCommon']['value'] == "Ortega":
-            dynamoResponse = table.get_item(Key = {'DiningCommon' : 'ortega'})
-            capacity = dynamoResponse['Item']['line']
-            capacity= str(capacity)
+            capacity = dynamoGet("ortega", "capacity")
             speech = buildSpeech("Ortega has a capacity of: " + capacity)
             #skill card
-            line = str(dynamoResponse['Item']['line'])
-            skillCardTitle = "More Information on Ortega"
-            skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
-            skillCard = createSkillCard(skillCardTitle, skillCardContent)
+            line = dynamoGet("ortega", "line")
+            skillCardTitle = "About Ortega"
+            skillCardContent = "There are " + line + " people in line.\n\nCongestion: " + "90%" + "\nAverage Eating Time: " + "30" + " min\nMenu: " + "taco"
         elif event['request']['intent']['slots']['diningCommon']['value'] == "carrillo":
-            dynamoResponse = table.get_item(Key = {'DiningCommon' : 'carrillo'})
-            capacity = dynamoResponse['Item']['line']
-            capacity= str(capacity)
+            capacity = dynamoGet("carrillo", "capacity")
             speech = buildSpeech("Carrillo has a capacity of: " + capacity)
             #skill card
-            line = str(dynamoResponse['Item']['line'])
-            skillCardTitle = "More Information on Carrillo"
-            skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
-            skillCard = createSkillCard(skillCardTitle, skillCardContent)
+            line = dynamoGet("carrillo", "line")
+            skillCardTitle = "About Carrillo"
+            skillCardContent = "There are " + line + " people in line.\n\nCongestion: " + "10%" + "\nAverage Eating Time: " + "35" + " min\nMenu: " + "quesadilla"
+        skillCard = createSkillCard(skillCardTitle, skillCardContent)
         return createResponse(speech, True, skillCard)
     
     if event['request']['intent']['name'] == "getLine":
         speech = buildSpeech("getLine request received, where's the slot?!")
         if event['request']['intent']['slots']['diningCommon']['value'] == "dlg":
-            dynamoResponse = table.get_item(Key = {'DiningCommon' : 'dlg'})
-            line = dynamoResponse['Item']['line']
-            line = str(line)
+            line = dynamoGet("dlg", "line")
             speech = buildSpeech("De La Guerra has a length of: " + line)
             # skill card
-            capacity = str(dynamoResponse['Item']['capacity'])
-            skillCardTitle = "More Information on De La Guerra"
-            skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
-            skillCard = createSkillCard(skillCardTitle, skillCardContent)
+            skillCardTitle = "This is the Line at De La Guerra:"
+            skillCardContent = "Picture from Dining Cam"
         elif event['request']['intent']['slots']['diningCommon']['value'] == "Ortega":
-            dynamoResponse = table.get_item(Key = {'DiningCommon' : 'ortega'})
-            line = dynamoResponse['Item']['line']
-            line = str(line)
+            line = dynamoGet("ortega", "line")
             speech = buildSpeech("The line at Ortega has a length of: " + line)
-            #skill card
-            capacity = str(dynamoResponse['Item']['capacity'])
-            skillCardTitle = "More Information on Ortega"
-            skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
-            skillCard = createSkillCard(skillCardTitle, skillCardContent)
+            # skill card
+            skillCardTitle = "This is the Line at Ortega:"
+            skillCardContent = "Picture from Dining Cam"
         elif event['request']['intent']['slots']['diningCommon']['value'] == "carrillo":
             dynamoResponse = table.get_item(Key = {'DiningCommon' : 'carrillo'})
-            line = dynamoResponse['Item']['line']
-            line = str(line)
+            line = dynamoGet("carrillo", "line")
             speech = buildSpeech("The line at Carillo has a length of: " + line)
-            #skill card
-            capacity = str(dynamoResponse['Item']['capacity'])
-            skillCardTitle = "More Information on Carrillo"
-            skillCardContent = "Capacity: " + capacity + "\nNumber of People in Line: " + line + "\n"
-            skillCard = createSkillCard(skillCardTitle, skillCardContent)
+            # skill card
+            skillCardTitle = "This is the Line at Carrillo:"
+            skillCardContent = "Picture from Dining Cam"
+        skillCard = createSkillCard(skillCardTitle, skillCardContent)
         return createResponse(speech,True,skillCard)
