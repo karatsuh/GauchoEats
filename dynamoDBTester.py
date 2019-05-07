e35303f6c3a0792ea35624b5d8ec0e84439e7a49
karatsuh <karatsuh@gmail.com>
2019-05-07 02:07:54 -0700

    in progress of converting lambda dynamo code to work locally

--- dynamoDBTester.py
+++ dynamoDBTester.py
@@ -1,1 +1,15 @@
 import boto3
+import json
+
+client = boto3.resource('dynamodb')
+table = client.Table('GauchoEats')
+
+def dynamoGet(DiningCommon, metric):
+    #PreCondition: DiningCommon and metric are both strings
+    #PostCondition: returns the wanted metric as a string
+    #DiningCommon = "dlg","carrillo","ortega"
+    #metric = "capacity","line"
+    dynamoResponse = table.get_item(Key = {'DiningCommon' : DiningCommon})
+    metric = dynamoResponse['Item'][metric]
+    metric = str(metric)
+    return metric
