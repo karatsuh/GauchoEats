30f42b441d195db6fe655ae9b04144841292f68d
karatsuh <karatsuh@gmail.com>
2019-05-09 20:53:51 -0700

    testing CI repo env variabales

--- dynamoDBTester.py
+++ dynamoDBTester.py
@@ -1,15 +1,14 @@
 import boto3
 import json
+#clientelle = boto3.client('dynamodb',aws_access_key_id='yyyy', aws_secret_access_key='xxxx', region_name='***')
+#table = client.Table('GauchoEats')
 
-client = boto3.resource('dynamodb')
-table = client.Table('GauchoEats')
-
-def dynamoGet(DiningCommon, metric):
-    #PreCondition: DiningCommon and metric are both strings
-    #PostCondition: returns the wanted metric as a string
-    #DiningCommon = "dlg","carrillo","ortega"
-    #metric = "capacity","line"
-    dynamoResponse = table.get_item(Key = {'DiningCommon' : DiningCommon})
-    metric = dynamoResponse['Item'][metric]
-    metric = str(metric)
-    return metric
+# def dynamoGet(DiningCommon, metric):
+#     #PreCondition: DiningCommon and metric are both strings
+#     #PostCondition: returns the wanted metric as a string
+#     #DiningCommon = "dlg","carrillo","ortega"
+#     #metric = "capacity","line"
+#     dynamoResponse = table.get_item(Key = {'DiningCommon' : DiningCommon})
+#     metric = dynamoResponse['Item'][metric]
+#     metric = str(metric)
+#     return metric
---
+++ envVariableTest.py
@@ -0,0 +1,2 @@
+print(AWS_ACCESS_KEY_ID)
+print(AWS_SECRET_ACCESS_KEY)
