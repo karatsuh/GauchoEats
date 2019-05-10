7a05c87dba444ab1656f263df455e35cc209af41
karatsuh <karatsuh@gmail.com>
2019-05-09 21:44:37 -0700

    alternate method of getting environment variables

--- envVariableTest.py
+++ envVariableTest.py
@@ -1,16 +1,18 @@
 import sys
-import pytest
+import os
+#import pytest
 
+print(os.environ['AWS_ACCESS_KEY_ID']
 #sys.argv[1] = Key
 #sys.argv[2] = secret
-
-def checker:
-    if str(sys.argv[1]) == "[secure]":
-        print("Not actually getting keys...")
-        return false
-    else:
-        print("Environment variables are being received!!")
-        return true
-
-
-assert( checker() == True)
+#
+# def checker:
+#     if str(sys.argv[1]) == "[secure]":
+#         print("Not actually getting keys...")
+#         return false
+#     else:
+#         print("Environment variables are being received!!")
+#         return true
+#
+#
+# assert( checker() == True)
