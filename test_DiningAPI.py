604e3dc466cf58bdf9ed1e65b3a22cc25c7c4df6
karatsuh <karatsuh@gmail.com>
2019-05-09 23:26:34 -0700

    testing getDate() with requests

--- test_DiningAPI.py
+++ test_DiningAPI.py
@@ -1,3 +1,15 @@
 import requests
 import pytest
 import json
+
+diningCamBaseUrl = os.environ['CAM_BASE_URL']
+diningBaseUrl = os.environ['DINING_BASE_URL']
+diningCamKey = os.environ['CAM_KEY']
+diningKey = os.environ['DINING_KEY']
+
+
+def getDate():
+    req = requests.get(diningBaseUrl + diningKey)
+    return req.json()[0]['code'] #assume the first available date is today
+
+print(getDate())
