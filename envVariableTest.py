bcc507dec3e74b28012d4b2bb53a3561038be68e
karatsuh <karatsuh@gmail.com>
2019-05-09 21:26:25 -0700

    testing actual values of env variables

--- envVariableTest.py
+++ envVariableTest.py
@@ -1,3 +1,8 @@
 import sys
+
 print(str(sys.argv[1]))
+if str(sys.argv[1]) == "[secure]":
+    print("Key is a rule key!")
+else:
+    print("Try out the actual dynamoDBTester!")
 #print(AWS_SECRET_ACCESS_KEY)
