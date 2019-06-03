360e0650264187dcb82e39e20968fa1aa6336af2
darhsu <35377472+darhsu@users.noreply.github.com>
2019-06-02 18:58:05 -0700

    Update test_objectDetection.py

--- test_objectDetection.py
+++ test_objectDetection.py
@@ -11,7 +11,8 @@ from boto3.dynamodb.conditions import Key, Attr
 from hoursFunctions import *
 
 client = boto3.resource('dynamodb',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-                        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name='us-east-1')
+                        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], 
+                        region_name='us-east-1')
 
 table = client.Table('GauchoEats')
 
