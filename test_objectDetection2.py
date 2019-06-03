d940d800742d96231123213fc4804a42e52ad611
darhsu <35377472+darhsu@users.noreply.github.com>
2019-06-02 19:06:26 -0700

    adding object detection test case

---
+++ test_objectDetection2.py
@@ -0,0 +1,26 @@
+#test_objectDection2.py
+
+
+import boto3
+import json
+import os
+import pytest
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+from hoursFunctions import *
+
+client = boto3.resource('dynamodb',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+                        region_name='us-east-1')
+
+table = client.Table('GauchoEats')
+
+def test_objectDection():
+    print("testing : object detection")
+    numArrived = 9
+    numLeft = 4
+    net = numArrived - numLeft
+    objectDection = net(numArrived, numLeft)
+    assert objectDection == 5
+
